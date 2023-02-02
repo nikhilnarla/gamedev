8718b98353de9444d7b6afc711602e408c4091c5
Mahima Mandhana <mmandhan@usc.edu>
2023-02-02 09:17:34 -0800

    Saved scene, removed commented code

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,29 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float jump;
-
     public float move;
-    public bool isJumping=false;
-    // Start is called before the first frame update
+    public bool isJumping = false;
+
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        // GameObject.Find("diamond").SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(speed * move, rb.velocity.y);
-
-        //GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
 
         if(Input.GetKeyDown("space") && !isJumping){
             rb.velocity =  new Vector2(rb.velocity.x, jump);
@@ -40,10 +33,4 @@ public class PlayerMovement : MonoBehaviour
             isJumping = false;
         }
     }
-
-    // private void OnCollisionExit2D(Collision2D other){
-    //     if(other.gameObject.CompareTag("Ground")){
-    //         isJumping = true;
-    //     }
-    // }
 }
