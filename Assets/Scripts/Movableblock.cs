9de2d17a800000104650cd0ff7bbe2ab9ba51974
nikhilnarla <63196625+nikhilnarla@users.noreply.github.com>
2023-02-04 19:23:37 -0800

    Added support for block bounce from right wall and prevented the movableblock from moving in upward direction when hit

--- Assets/Scripts/Movableblock.cs
+++ Assets/Scripts/Movableblock.cs
@@ -5,9 +5,33 @@ public class Movableblock : MonoBehaviour
 
     public GameObject key;
     public AnalyticsManager analyticsManager;
-
     bool keyFound = false;
-    
+    private Rigidbody2D rb;
+    public float move;
+    public float speed;
+    public Vector3 startPosition;
+    public bool onFloor = false;    
+
+   void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+         startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = rb.velocity; //get the velocity
+        v.y = v.y > 0 ? 0 : v.y; //set to 0 if positive
+        if (v.y == 0 && !onFloor)
+        {
+            transform.position = startPosition;
+        }
+        rb.velocity = v; //apply the velocity
+
+    }
+
+
     void OnCollisionEnter2D(Collision2D col)
     {
         
@@ -23,6 +47,17 @@ public class Movableblock : MonoBehaviour
             analyticsManager.SendEvent("LEVEL1 KEYFOUND");
             keyFound = true;
             }
+        }
+        else if (col.gameObject.name == "rightcollider")
+        {
+            Debug.Log("collision right wall!");
+            
+            rb.velocity = new Vector2(-1,0);
+        }
+
+        else if (col.gameObject.name == "Square")
+        {
+            onFloor = true;
         }
     }
 
