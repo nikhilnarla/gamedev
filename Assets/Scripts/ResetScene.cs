45c658ebaa798b1c542727e447a3788be09e9068
Ashley Hau <ashhau@gmail.com>
2023-02-05 11:45:13 -0800

    no friction & reset button added
    - capsule should not be sticking to the sides of the blocks
    - reset button should reset all the blocks in its place and place character back at the start

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    public void resetScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
