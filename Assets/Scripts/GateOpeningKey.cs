fd153af9ed816a81bd8987eb0e8a1b0f73081c50
yashbujji <36237332+yashbujji@users.noreply.github.com>
2023-02-11 07:37:13 -0800

    AnalyticsManager for level-1 latest

--- Assets/Scripts/GateOpeningKey.cs
+++ Assets/Scripts/GateOpeningKey.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class GateOpeningKey : MonoBehaviour
 {
     public GameObject key;
+    public AnalyticsManager analyticsManager;
 
     void OnCollisionEnter2D(Collision2D col)
     {
 
         if (col.gameObject.name == "Capsule" && key.activeSelf)
         {
+             //Analytics event - key Collected
+            analyticsManager.SendEvent("LEVEL1 KEYCOLLECTED");
             Debug.Log("collision key!");
             Destroy(GameObject.Find("Gate"));
             Destroy(GameObject.Find("Diamond"));
