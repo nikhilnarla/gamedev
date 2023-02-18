8970c9bed8c3ecefcd05ccc3142b4919a70a0303
blm3886 <benficax@usc.edu>
2023-02-18 10:50:42 -0800

    added fire

---
+++ Assets/Scripts/FlippingTile2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlippingTile2 : MonoBehaviour
+{
+  public Rigidbody2D flipBlock;
+  public float rotationSpeed;
+  private float rotZ;
+
+  void Update(){
+      rotZ -= Time.deltaTime * rotationSpeed;
+      transform.rotation = Quaternion.Euler(0,0,rotZ);
+  }
+}
