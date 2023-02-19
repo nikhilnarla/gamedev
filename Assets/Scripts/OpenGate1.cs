9e5e9c7e5cf5ffd20d7e12f6304459bfe153b4be
yashbujji <36237332+yashbujji@users.noreply.github.com>
2023-02-19 01:47:45 -0800

    keys collected analytics levle6

--- Assets/Scripts/OpenGate1.cs
+++ Assets/Scripts/OpenGate1.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class OpenGate1 : MonoBehaviour
 {
+    public AnalyticsManager analyticsManager;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -9,6 +11,7 @@ public class OpenGate1 : MonoBehaviour
             Debug.Log("Gate 1 Opened!");
             Destroy(GameObject.Find("Gate1OpenKey"));
             Destroy(GameObject.Find("Gate1"));
+            analyticsManager.SendEvent("LEVEL6 GATE1 OPENED");
         }
     }
 }
