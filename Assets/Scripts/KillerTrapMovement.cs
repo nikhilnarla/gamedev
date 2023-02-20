efb04270acfe7eef285f356f10a77118cdea23eb
nikhilkanatala <kanatalanikhil25@gmail.com>
2023-02-19 18:50:54 -0800

    Extra Additions

---
+++ Assets/Scripts/KillerTrapMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillerTrapMovement : MonoBehaviour
+{
+    private float TimetoToggle = 5f;
+    private float currentTime = 0f;
+
+    [SerializeField] private GameObject Trap;
+
+    void Update()
+    {
+
+    }
+}
