665484cc4603100071a7f92e520f6fc86f644242
Mahima Mandhana <mmandhan@usc.edu>
2023-02-19 20:02:47 -0800

    webgl build and some updates

--- Assets/Scripts/PlayerMovement6.cs
+++ Assets/Scripts/PlayerMovement6.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerMovement6 : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    public AnalyticsManager analyticsManager;
+
 
     public float speed;
     public float jump;
     public float move;
     public bool isJumping = false;
+    public bool showText = false;
+    public GameObject frozenKey;
 
     void Start()
     {
@@ -34,6 +43,19 @@ public class PlayerMovement6 : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jump);
             isJumping = true;
         }
+
+        Flip();
+    }
+
+    void Flip() {
+        if (Input.GetKeyDown("left") && IsGrounded())
+        {
+            transform.Rotate(0, 180, 0);
+        }
+        // if (Input.GetKeyDown("right") && IsGrounded())
+        // {
+        //     transform.Rotate(0, 0, 0);
+        // }
     }
 
     private bool IsGrounded()
@@ -43,17 +65,82 @@ public class PlayerMovement6 : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+
         if (other.gameObject.CompareTag("Ground"))
         {
             isJumping = false;
         }
+         if (other.gameObject.tag == ("LavaParticle"))
+        {
+            analyticsManager.SendEvent("LEVEL6 PLAYER FELL INTO LAVA");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        // if (other.gameObject.name == ("Ground")) // from wk 5
+        // {
+        //     isJumping = false;
+        // }
+        if(other.gameObject.name == "SuperPowerKey")
+        {
+            Destroy(GameObject.Find("SuperPowerKey"));
+            Destroy(GameObject.Find("SuperPowerGate"));
+        }
+
         if (other.gameObject.name == "Portal1")
         {
+            analyticsManager.SendEvent("LEVEL6 PORTAL1 USED");
             rb.transform.position = new Vector2(4.7f, 2.8f);
         }
         if (other.gameObject.name == "Portal2")
         {
+            analyticsManager.SendEvent("LEVEL6 PORTAL2 USED");
             rb.transform.position = new Vector2(-2.8f, -0.671f);
+        }
+
+        if(other.gameObject.name == "SuperKey")
+        {
+            Destroy(GameObject.Find("SuperKey"));
+            showText = true;
+            StartCoroutine(WaitAndMakeTextDisappear(3));
+        }
+        if(other.gameObject.name == "KeyReveal"){
+           //Debug.Log("KR");
+           Destroy(GameObject.Find("KeyReveal"));
+           frozenKey.SetActive(true);
+           GameObject.Find("Gate2OpenKey").GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if(other.gameObject.name == "EndGate1"){
+          Destroy(GameObject.Find("EndGate1"));
+          analyticsManager.SendEvent("LEVEL6 END ");
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+        if(other.gameObject.name == "EndGate2"){
+          Destroy(GameObject.Find("EndGate2"));
+          analyticsManager.SendEvent("LEVEL6 END");
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+
+    IEnumerator WaitAndMakeTextDisappear(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        showText = false;
+    }
+
+    void OnGUI()
+    {
+        if(showText) {
+            // text - red color
+            GUIStyle style = new GUIStyle(GUI.skin.label);
+            style.alignment = TextAnchor.MiddleCenter;
+            style.normal.textColor = Color.red;
+
+            // draw background - black color
+            Rect rect = new Rect(Screen.width / 2 - 60, Screen.height / 2 - 25, 120, 70);
+            GUI.color = Color.black;
+            GUI.DrawTexture(rect, Texture2D.whiteTexture);
+            GUI.color = Color.white;
+
+            GUI.Label(rect, "Powerup Collected, Press C to shoot bullets!", style);
         }
     }
 }
