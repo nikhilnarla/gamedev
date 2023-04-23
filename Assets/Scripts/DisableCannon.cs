6cc976c52095b14c1bbe609f7d9bb46768fb576f
benfica_19 <benficax@usc.edu>
2023-04-23 01:23:12 -0700

    updates in level4

---
+++ Assets/Scripts/DisableCannon.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCannon : MonoBehaviour
+{
+    public GunShooting Cannon;
+    public GameObject CannonBase;
+    public GameObject CannonShooter;
+    public GameObject CannonAlarm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+
+        if (other.gameObject.name.Equals("Green Block"))
+        {
+            Cannon.Range = 1;
+            CannonBase.GetComponent<Renderer>().material.color = Color.gray;
+            CannonShooter.GetComponent<Renderer>().material.color = Color.gray;
+            CannonAlarm.GetComponent<Renderer>().material.color = Color.gray;
+        }
+
+    }
+}
